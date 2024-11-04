c23ab7bc7d79d1d0994147f200af56371fab1ee7
Elijah Wright <elw@stderr.org>
2024-11-04 16:14:22 -0600

    Update utils.bash

--- lib/utils.bash
+++ lib/utils.bash
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for chefvm.
-GH_REPO="https://github.com/elijah/mise-chefvm"
+GH_REPO="https://github.com/torbrock/chefvm"
 TOOL_NAME="chefvm"
 TOOL_TEST="chefvm version"
 
